36b9c6150ce67de1a5b916f3a18574f03e580c9c
mohammedkari <101977018+mohammedkari@users.noreply.github.com>
2022-07-26 20:30:03 +0000

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2019 Brought To You By <a href="https://code-projects.org/">Karim</a></strong>
+</footer>
